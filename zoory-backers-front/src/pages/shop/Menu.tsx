fbe77f12ea84c7e1c906055f3b0ced62e1b70f31
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2023-12-30 16:18:30 +0530

    pagination & menu section

--- zoory-backers-front/src/pages/shop/Menu.tsx
+++ zoory-backers-front/src/pages/shop/Menu.tsx
@@ -1,34 +1,36 @@
 import {useEffect, useState} from "react";
-import Cards from "../../component/specialdishes/Cards.tsx";
 import {FaFilter} from "react-icons/fa";
-
+import Cards from "../../component/specialdishes/Cards.tsx";
 
 const Menu = () => {
     const [menu,setMenu]=useState([]);
     const[filterdItems,setFilterdItems]=useState([]);
     const [selectedCategory,setSelectedCategory]=useState("all");
     const [sortOption,setSortOption]=useState("default");
+    const [currentPage,setCurrentPage]=useState(1);
+    const [itemsPerPage]=useState(8);
+
 
     {/*start loading data*/}
-     useEffect(()=>{
-         {/*start fetch data from the back end*/}
-         const fetchData = async ()=>{
-             try{
-                 const response=await fetch("/menu.json");
-                 const data=await response.json();
+    useEffect(()=>{
+        {/*start fetch data from the back end*/}
+        const fetchData = async ()=>{
+            try{
+                const response=await fetch("/menu.json");
+                const data=await response.json();
                 // console.log(data)
-                 setMenu(data);
-                 setFilterdItems(data);
-             }catch (error){
-                 console.log("Error Fetching Data",error)
-             }
+                setMenu(data);
+                setFilterdItems(data);
+            }catch (error){
+                console.log("Error Fetching Data",error)
+            }
 
-         };
-         {/*end fetch data from the back end*/}
-         //call the function
-            fetchData();
+        };
+        {/*end fetch data from the back end*/}
+        //call the function
+        fetchData();
 
-     },[])
+    },[])
     {/*end loading data*/}
 
     {/*start filtering data based on category*/}
@@ -37,6 +39,7 @@ const Menu = () => {
 
         setFilterdItems(fillterd);
         setSelectedCategory(category)
+        setCurrentPage(1);
 
     };
 
@@ -45,43 +48,51 @@ const Menu = () => {
     const showAll = () =>{
         setFilterdItems(menu);
         setSelectedCategory("all");
+        setCurrentPage(1);
 
     }
     {/*end show all data*/}
 
     {/*start sorting based on A-Z , Z-A, Low-High pricing*/}
-      const handleSortChange =(option)=>{
+    const handleSortChange =(option)=>{
         setSortOption(option)
 
-          let sortedItems =[...filterdItems];
+        let sortedItems =[...filterdItems];
 
         //logic
-          switch (option){
-              case "A-Z" :
-                  sortedItems.sort((a, b) => a.name.localeCompare(b.name))
-                  break;
-              case "Z-A" :
-                  sortedItems.sort((a, b) => b.name.localeCompare(a.name))
-                  break;
-              case "low-to-high" :
-                  sortedItems.sort((a, b) => a.price - b.price)
-                  break;
-              case "high-to-low" :
-                  sortedItems.sort((a, b) => b.price - a.price)
-                  break;
-              default : break;
-          }
+        switch (option){
+            case "A-Z" :
+                sortedItems.sort((a, b) => a.name.localeCompare(b.name))
+                break;
+            case "Z-A" :
+                sortedItems.sort((a, b) => b.name.localeCompare(a.name))
+                break;
+            case "low-to-high" :
+                sortedItems.sort((a, b) => a.price - b.price)
+                break;
+            case "high-to-low" :
+                sortedItems.sort((a, b) => b.price - a.price)
+                break;
+            default : break;
+        }
 
-          setFilterdItems(sortedItems);
+        setFilterdItems(sortedItems);
+        setCurrentPage(1);
 
 
 
 
-      }
+    }
     {/*end sorting based on A-Z , Z-A, Low-High pricing*/}
 
 
     {/*end filtering data based on category*/}
+
+    //pagination logic
+    const indexOfLastItem =currentPage*itemsPerPage;
+    const indexOfFirstItem=indexOfLastItem-itemsPerPage;
+    const currentItems=filterdItems.slice(indexOfFirstItem,indexOfLastItem);
+    const paginate=(pageNumber) => setCurrentPage(pageNumber);
 
     return (
         <div className="mt-10" >
@@ -113,7 +124,7 @@ const Menu = () => {
                     <div className="flex flex-row justify-start md:items-center md:gap-8 gap-4 flex-wrap">
                         {/*all categories btns*/}
                         <button onClick={showAll}
-                        className={selectedCategory === "all" ? "active" : ""}
+                                className={selectedCategory === "all" ? "active" : ""}
                         >All</button>
                         <button onClick={() => filterItems("salad")}
                                 className={selectedCategory === "salad" ? "active" : ""}
@@ -135,13 +146,13 @@ const Menu = () => {
 
                     {/*start sorting base filtering*/}
                     <div className="flex justify-end mb-4 rounded-sm">
-                       <div className=" bg-black p-2">
-                           <FaFilter className="h-4 w-4 text-white"/>
-                       </div>
+                        <div className=" bg-black p-2">
+                            <FaFilter className="h-4 w-4 text-white"/>
+                        </div>
 
                         {/*start sorting option button*/}
                         <select name="sort" id="sort"
-                        onChange={(e) =>handleSortChange(e.target.value)}
+                                onChange={(e) =>handleSortChange(e.target.value)}
                                 value={sortOption}
                                 className="bg-black text-white px-2 py-1 rounded-sm" >
                             <option value="default">Default</option>
@@ -149,7 +160,7 @@ const Menu = () => {
                             <option value="Z-A">Z-A</option>
                             <option value="low-to-high">Low to High</option>
                             <option value="high-to-low">High to Low</option>
-                       </select>
+                        </select>
                         {/*end sorting option button*/}
                     </div>
                     {/* end sorting base filtering*/}
@@ -161,7 +172,7 @@ const Menu = () => {
                 {/*start product cards*/}
                 <div className="grid md:grid-cols-3 sm:grid-cols-2 grid-cols-1 gap-4">
                     {
-                        filterdItems.map((item) =>(
+                        currentItems.map((item) =>(
                             <Cards key={item._id} item={item}/>
                         ))
                     }
@@ -170,6 +181,20 @@ const Menu = () => {
 
             </div>
             {/*end menu shop section*/}
+
+            {/*start pagination section*/}
+            <div className="flex justify-center my-8">
+                {
+                    Array.from({length:Math.ceil(filterdItems.length / itemsPerPage)}).map((_,index) =>(
+                        <button
+                            key={index + 1}
+                            onClick={() => paginate(index + 1)}
+                            className={`mx-1 px-3 py-1 rounded-full ${currentPage === index +1 ? "bg-[#FF9800] text-white":"bg-gray-200"}`}
+                        > {index + 1}  </button>
+                    ) )
+                }
+            </div>
+            {/*end pagination section*/}
         </div>
     );
 };
