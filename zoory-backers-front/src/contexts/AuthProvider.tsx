b3ba484c43199cf64ff46495f1341fec18c62295
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-01-01 07:54:22 +0530

    signup with gmail

--- zoory-backers-front/src/contexts/AuthProvider.tsx
+++ zoory-backers-front/src/contexts/AuthProvider.tsx
@@ -1,9 +1,12 @@
 import {createContext, useState} from "react";
-import { createUserWithEmailAndPassword,getAuth } from "firebase/auth";
+import { GoogleAuthProvider,createUserWithEmailAndPassword,getAuth } from "firebase/auth";
 import app from "../firebase/firebase.config.js";
+
+
 
 export const AuthContext =createContext();
 const auth = getAuth(app);
+const googleProvider = new GoogleAuthProvider();
 const AuthProvider = ({children}) => {
     const [user,setUser]=useState(null);
     const[loading,setLoading]=useState(true);
@@ -15,11 +18,18 @@ const AuthProvider = ({children}) => {
    }
     {/*end create an account*/}
 
+    {/*start sign up with gmail*/}
+    const signUpWithGmail =()=>{
+    return    signInWithPopup(auth, googleProvider)
+    }
+    {/*end sign up with gmail*/}
 
+    {/*login using email & password*/}
 
     const authInfo ={
         user,
-        createUser
+        createUser,
+        signUpWithGmail
     }
     return (
         <AuthContext.Provider value={authInfo}>
