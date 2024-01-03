68401e68bc3eebd1fbc60ef12aeda70e55bc23af
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-01-03 07:33:45 +0530

    start middlewhare

--- zoory-backers-back/index.js
+++ zoory-backers-back/index.js
@@ -1,7 +1,12 @@
 const express = require('express')
-const app = express()
+const app = express();
+const cors=require('cors')
 const port = process.env.PORT || 6001;
 
+{/*start middleware*/}
+   app.use(cors());
+    app.use(express.json())
+{/*end middleware*/}
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
