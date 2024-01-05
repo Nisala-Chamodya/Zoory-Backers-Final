d6accc92f9b47e33636c59179e8ccdc9265bfbbf
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-01-05 10:59:29 +0530

    get all menu data item wise

--- zoory-backers-front/src/component/specialdishes/Cards.tsx
+++ zoory-backers-front/src/component/specialdishes/Cards.tsx
@@ -1,10 +1,29 @@
 import {Link} from "react-router-dom";
-import {useState} from "react";
+import {useContext, useState} from "react";
 import {FaHeart} from "react-icons/fa";
+import { AuthContext } from "../../contexts/AuthProvider";
 
 const Cards = ({item}) => {
+    const{name,image,price,recipe,_id} =item
     {/*start hart fill*/}
     const[isHeartFilled,setIsHeartFilled]=useState(false);
+    //start config user
+    const{user} =useContext(AuthContext);
+   // console.log(user);
+    
+
+    {/*start  add to cart btn*/}
+    const handleAddtoCart =(item) =>{
+     //   console.log("handle cart",item);
+     if(user && user?.email){
+        const cartItem={menuItemId:_id,name,quantiy:1,image,price,email:user.email}
+        console.log(cartItem);
+        
+     }
+        
+    
+     {/*end  add to cart btn*/}
+    }
 
     const handleHeartClicked = () =>{
         setIsHeartFilled(!isHeartFilled)
@@ -13,23 +32,23 @@ const Cards = ({item}) => {
     return (
 
 
-            <div className="card w-96 bg-base-100 shadow-xl relative">
+            <div className="relative shadow-xl card w-96 bg-base-100">
                 {/*start handle hart*/}
                 <div className={`rating gap-1 absolute right-2 top-2 p-4 heartStar bg-[#FF9800] ${isHeartFilled ?
                     "text-rose-500" : "text-black"}`} onClick={handleHeartClicked}>
-                    <FaHeart className="h-5 w-5 cursor-pointer"/>
+                    <FaHeart className="w-5 h-5 cursor-pointer"/>
                 </div>
                 {/*end handle hart*/}
                 {/*start desiui card*/}
                <Link to={`/menu/${item._id}`}>
-                   <figure><img src={item.image} alt={item.name}  className="hover:scale-75 transition-all duration-200 md:h-72"/></figure>
+                   <figure><img src={item.image} alt={item.name}  className="transition-all duration-200 hover:scale-75 md:h-72"/></figure>
                </Link>
                 <div className="card-body">
                    <Link to={`/menu/${item._id}`}> <h2 className="card-title">{item.name}</h2></Link>
                     <p>Description of the item</p>
-                    <div className="card-actions justify-between items-center mt-2">
+                    <div className="items-center justify-between mt-2 card-actions">
                         <h5 className="font-semibold"><span className="text-sm text-red">R.S.</span> {item.price}</h5>
-                        <button className="btn bg-[#FF9800] text-white">Buy Now</button>
+                        <button className="btn bg-[#FF9800] text-white" onClick={() => handleAddtoCart(item)}>Add to Cart</button>
                     </div>
                 </div>
                 {/*end desiui card*/}
