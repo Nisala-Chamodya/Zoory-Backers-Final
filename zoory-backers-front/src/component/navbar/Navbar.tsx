955266768a93a14ce927f8e1ea18f1d475242abe
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-01-09 08:37:13 +0530

    dynamic cart button

--- zoory-backers-front/src/component/navbar/Navbar.tsx
+++ zoory-backers-front/src/component/navbar/Navbar.tsx
@@ -6,6 +6,7 @@ import Model from "../model/Model.tsx";
 import { AuthContext } from '../../contexts/AuthProvider.tsx';
 import Profile from '../profile/Profile.tsx';
 import { Link } from 'react-router-dom';
+import useCart from '../../hooks/useCart.tsx';
 
 
 
@@ -17,7 +18,11 @@ const Navbar = () => {
 
    const {user}=useContext(AuthContext)
    console.log(user);
-   
+
+   {/*start useCartHooks */}
+ const[cart,refetch]=useCart();
+// console.log(cart);
+   {/*end useCartHooks */}
 
     useEffect(() => {
         // Define the scroll event handler
@@ -106,7 +111,7 @@ const Navbar = () => {
                     <div tabIndex={0} role="button" className="items-center justify-center hidden mr-3 btn btn-ghost btn-circle lg:flex ">
                             <div className="indicator">
                                 <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" /></svg>
-                                <span className="badge badge-sm indicator-item">0</span>
+                                <span className="badge badge-sm indicator-item">{cart.length || 0}</span>
                             </div>
                         </div>
                         </Link>
