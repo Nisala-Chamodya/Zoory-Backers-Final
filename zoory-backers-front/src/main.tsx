0f8113fa106db83c7e3bc13356ec82f8eaeb014c
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-01-09 07:18:05 +0530

    add tankstack

--- zoory-backers-front/src/main.tsx
+++ zoory-backers-front/src/main.tsx
@@ -7,11 +7,27 @@ import {RouterProvider} from "react-router-dom";
 import router from "./router/Router.tsx";
 import AuthProvider from './contexts/AuthProvider.tsx';
 
+{/*start tankstack */}
+import {
+
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+
+// Create a client
+const queryClient = new QueryClient()
+{/*end tankstack */}
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-        <AuthProvider>
-            <RouterProvider router={router} />
+
+     <AuthProvider>
+            <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
         </AuthProvider>
+        
 
     
 
