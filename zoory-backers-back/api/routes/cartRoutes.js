c1d36adfe9fd1a4e7f4058d71ab84ebe1c99ec88
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-01-23 14:36:47 +0530

    create userSchema

--- zoory-backers-back/api/routes/cartRoutes.js
+++ zoory-backers-back/api/routes/cartRoutes.js
@@ -7,6 +7,6 @@ router.get('/',cartController.getCartByEmail);
 router.post('/',cartController.addToCart);
 router.delete('/:id',cartController.deleteCart);
 router.put('/:id',cartController.updateCart);
-
+router.get('/:id',cartController.getSingleCart);
 
 module.exports=router;
