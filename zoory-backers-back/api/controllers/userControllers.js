6bbde1f1d3d587f43cfb9330cc8066aa08f926ee
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-01-23 16:48:58 +0530

    create users route implement

--- zoory-backers-back/api/controllers/userControllers.js
+++ zoory-backers-back/api/controllers/userControllers.js
@@ -15,6 +15,26 @@ const getAllUsers =async (req, res)=>{
 }
 //end get all users
 
+//start post a new user
+const createUser= async (req,res)=>{
+    const user=req.body;
+    const query={email:user.email}
+
+    try {
+        const existingUser =await User.findOne(query);
+        if (existingUser){
+            return res.status(302).json({message:"User Already Exists"});
+        }
+        const result=await User.create(user);
+        res.status(200).json(result);
+
+    }catch (error){
+        res.status(500).json({message:error.message});
+    }
+};
+//end post a New user
+
 module.exports={
-    getAllUsers
+    getAllUsers,
+    createUser
 }
--- zoory-backers-back/api/routes/UserRoutes.js
+++ zoory-backers-back/api/routes/UserRoutes.js
@@ -3,6 +3,7 @@ const  router= express.Router();
 
 const userController=require('../controllers/userControllers')
 
-router.get('/',userController.getAllUsers)
+router.get('/',userController.getAllUsers);
+router.post('/',userController.createUser)
 
 module.exports=router;
