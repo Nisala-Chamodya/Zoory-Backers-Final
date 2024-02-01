63fa6737b06be2d658f357d746ac26e8ff772740
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-02-01 15:57:07 +0530

    get admin & make admin

--- zoory-backers-back/api/controllers/userControllers.js
+++ zoory-backers-back/api/controllers/userControllers.js
@@ -52,8 +52,56 @@ const deletedUser=await User.findByIdAndDelete(userId);
 };
 //end a delete user
 
+//start get admin
+const getAdmin = async (req,res)=>{
+const email=req.params.email;
+const query={email:email}
+
+    try {
+    const user=await User.findOne(query);
+        console.log(user)
+    if (email !== req.decoded.email){
+        return res.status(403).send({message:"Forbidden Access"})
+    }
+    let admin =false;
+    if (user){
+        admin =user?.role === 'admin'
+    }
+    res.status(200).json({admin})
+
+    }catch (error){
+        res.status(500).json({message:error.message});
+    }
+}
+//end get admin
+
+//start make admin of a user
+const makeAdmin=async(req,res)=>{
+    const userId=req.params.id;
+    const {name,email,photoURL,role}=req.body;
+
+    try {
+        const updatedUser=await User.findByIdAndUpdate(
+            userId,
+            {role:"admin"},
+            {new:true,runValidators:true}
+        );
+        if (!updatedUser){
+            return res.status(404).json({message:"User Not Found"});
+        }
+        res.status(200).json(updatedUser)
+    }catch (error){
+        res.status(500).json({message:error.message});
+    }
+
+}
+//end make admin of a user
+
 module.exports={
     getAllUsers,
     createUser,
-    deleteUser
+    deleteUser,
+    getAdmin,
+    makeAdmin
+
 }
