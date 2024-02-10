6c7d87971e6323494b739641c7824f6679eb9968
Nisala - Chamodya <113490575+Nisala-Chamodya@users.noreply.github.com>
2024-02-10 11:23:39 +0530

    Create Manage All Menu Item Ui design

--- zoory-backers-front/src/pages/dashboard/admin/ManageItem.tsx
+++ zoory-backers-front/src/pages/dashboard/admin/ManageItem.tsx
@@ -1,5 +1,66 @@
+import { Link } from "react-router-dom";
+import useMenu from "../../../hooks/useMenu";
+import { FaEdit, FaTrashAlt } from "react-icons/fa";
+
 const ManageItem = () => {
-  return <div>ManageItem</div>;
+  const [menu, refetch] = useMenu();
+  console.log(menu);
+  return (
+    <div className="w-full md:w-[870px] px-4 mx-auto">
+      <h2 className="my-4 text-2xl font-semibold">
+        Manage All <span className="text-orange">Menu Items</span>
+      </h2>
+      {/*start menu item table */}
+      <div>
+        <div className="overflow-x-auto">
+          <table className="table">
+            {/* head */}
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Image</th>
+                <th>Item Name</th>
+                <th>Prices</th>
+                <th>Edit</th>
+                <th>Delete</th>
+              </tr>
+            </thead>
+            <tbody>
+              {menu.map((item, index) => (
+                <tr key={index}>
+                  <th>{index + 1}</th>
+                  <td>
+                    <div className="flex items-center gap-3">
+                      <div className="avatar">
+                        <div className="w-12 h-12 mask mask-squircle">
+                          <img src={item.image} alt={item.name} />
+                        </div>
+                      </div>
+                    </div>
+                  </td>
+                  <td>{item.name}</td>
+                  <td>R.s. {item.price}</td>
+                  <td>
+                    <Link to={`/dashboard/update-menu/${item._id}`}>
+                      <button className="text-white btn btn-ghost btn-xs bg-orange">
+                        <FaEdit />
+                      </button>
+                    </Link>
+                  </td>
+                  <td>
+                    <button className="text-white btn btn-ghost btn-xs bg-red">
+                      <FaTrashAlt />
+                    </button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+      {/*end menu item table */}
+    </div>
+  );
 };
 
 export default ManageItem;
